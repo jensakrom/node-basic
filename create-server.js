8f0d8db7ea26a44c28842f30f1df01ea26bb4f34
jensakrom <jensakrom@gmail.com>
2019-03-19 10:05:59 +0700

    serving html to browser wiht pipe

--- create-server.js
+++ create-server.js
@@ -3,20 +3,29 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = '3000';
 const constant = require('./constant');
+const pathHtml = '/view/index.html';
 
-const readStream = fs.createReadStream(__dirname + constant.READ_ME, 'utf8');
+// Read plain/text
+// const readStream = fs.createReadStream(__dirname + constant.READ_ME, 'utf8');
+
+// Read html
+const readStream = fs.createReadStream(__dirname + pathHtml, 'utf8');
 const writeStream = fs.createWriteStream(__dirname + constant.WRITE_ME);
 
 const server = http.createServer((req, res) => {
     // console.log(req.url);
-    // res.statusCode = '200';
-    // res.setHeader('Content-type', 'text/plain');
-    res.writeHead(200, {'Content-type':'text/plain'});
+
+    // write text and plain
+    // res.writeHead(200, {'Content-type':'text/plain'});
+
+    // Write index html
+    res.writeHead(200, {'Content-type':'text/html'});
     
-    // Add stream chunk to pipe and send to browser
+    // Add stream chunk to pipe and send to browser (write text palin and html)
     readStream.pipe(res);
     readStream.pipe(writeStream);
     // res.end('Hallo...')
+
 });
 
 server.listen(port, hostname, () => {
